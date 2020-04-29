8f8705bec3035a46ae2e65e7d34574855883465b
roy-subrata <roy.subrat.cs@gmail.com>
2020-04-29 11:22:42 +0600

    adding google auth functionality

--- Auth/Startup.cs
+++ Auth/Startup.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Security.Claims;
@@ -10,17 +12,34 @@ namespace Auth
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            services.AddControllersWithViews(o => o.Filters.Add(new AuthorizeFilter()));
+            services.AddAuthentication(o =>
+            {
+                o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+            })
                 .AddCookie("Cookies", config =>
                  {
+
                      config.LoginPath = "/Auth/Login";
                      config.Cookie.Name = "Granma.Cookies";
                      //config.Events = new CookieAuthenticationEvents { OnValidatePrincipal = async (c) => { c.Principal.HasClaim(f=>f.Type==ClaimTypes.Name); } };
-                 });
-            services.AddControllersWithViews(o => o.Filters.Add(new AuthorizeFilter()));
+                 })
+                .AddGoogle(o =>
+                {
+                    o.ClientId = _configuration["Google:ClientId"];
+                    o.ClientSecret = _configuration["Google:ClientSecret"];
+                });
+
         }
 
 
